2457a3a78479dcba0ee0b655880f75dd89d55ba5
sniperdevil <muge111hhh@gmail.com>
2020-03-13 18:54:46 +0530

    .

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,9 @@
 const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
-    '/api',
+    '/',
     proxy({
-      target: 'http://localhost:5000',
+      target: 'http://royal-stats.herokuapp.com',
       changeOrigin: true,
     })
   );
